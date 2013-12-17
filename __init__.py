2f827b66184a885d2a203fd6c26b5dcef407dcae
Charles Connell <charles@connells.org>
2013-12-17 18:33:34 -0500

    Don't get stuck on plain text input

--- __init__.py
+++ __init__.py
@@ -34,13 +34,16 @@ def ENMLToHTML(content, pretty=True, **kwargs):
             media.replace_with(new_tag)
     
     note = soup.find('en-note')
-    body = soup.new_tag('body')
-    html = soup.new_tag('html')
-    html.append(note)
-    note.name = 'body'
+    if note:
+      body = soup.new_tag('body')
+      html = soup.new_tag('html')
+      html.append(note)
+      note.name = 'body'
 
-    output = html.prettify().encode('utf-8') if pretty else str(html)
-    return output
+      output = html.prettify().encode('utf-8') if pretty else str(html)
+      return output
+      
+    return content
 
 
 class MediaStore(object):
